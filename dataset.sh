b55f9bdff63f3357adabf70ca1f695ba3ae50085
Bardiya Basafa <Bardiya.Basafa@gmail.com>
2026-07-04 14:07:27 +0330

    base: preprocess

--- dataset.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl -L -o {pwd}/text-classification-documentation.zip\
-  https://www.kaggle.com/api/v1/datasets/download/tanishqdublish/text-classification-documentation
--- main.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("tanishqdublish/text-classification-documentation")
-
-print("Path to dataset files:", path)
